802ae3e9f74c1c1764f4d333dc3d2b3e361e7eca
Shivank Kacker <kacker.shivank@gmail.com>
2022-10-12 01:51:40 +0530

    for showcase

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -1,26 +1,30 @@
-let todoList = require("../index");
+let todoList = require("../todo");
 
 const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
 /* eslint-disable no-undef */
 describe("Todo List Test Suite", () => {
+  beforeAll(() => {
+    add({ title: "Buy milk", dueDate: "2020-12-31", completed: false });
+  });
+
   test("Create a new Todo", () => {
-    expect(all.length).toEqual(0);
+    expect(all.length).toEqual(1);
     add({
       title: "A test item",
       completed: false,
       dueDate: new Date().toLocaleDateString("en-CA"),
     });
 
-    expect(all.length).toEqual(1);
+    expect(all.length).toEqual(2);
   });
   test("Should mark a Todo as complete", () => {
-    expect(all.length).toEqual(1);
+    expect(all.length).toEqual(2);
     expect(all[0].completed).toEqual(false);
     markAsComplete(0);
     expect(all[0].completed).toEqual(true);
   });
   test("Should return overdue items", () => {
-    expect(all.length).toEqual(1);
+    expect(all.length).toEqual(2);
     const today = new Date();
     const oneDay = 60 * 60 * 24 * 1000;
     add({
@@ -30,24 +34,24 @@ describe("Todo List Test Suite", () => {
         "en-CA"
       ),
     });
-    expect(all.length).toEqual(2);
+    expect(all.length).toEqual(3);
     overdueItems = overdue();
-    expect(overdueItems.length).toBe(1);
+    expect(overdueItems.length).toBe(2);
   });
   test("Should return due today items", () => {
-    expect(all.length).toEqual(2);
+    expect(all.length).toEqual(3);
     const today = new Date();
     add({
       title: "A due today test item",
       completed: false,
       dueDate: new Date(today.getTime()).toLocaleDateString("en-CA"),
     });
-    expect(all.length).toEqual(3);
+    expect(all.length).toEqual(4);
     dueTodayItems = dueToday();
     expect(dueTodayItems.length).toBe(2);
   });
   test("Should return due later items", () => {
-    expect(all.length).toEqual(3);
+    expect(all.length).toEqual(4);
     const today = new Date();
     const oneDay = 60 * 60 * 24 * 1000;
     add({
@@ -57,7 +61,7 @@ describe("Todo List Test Suite", () => {
         "en-CA"
       ),
     });
-    expect(all.length).toEqual(4);
+    expect(all.length).toEqual(5);
     dueLaterItems = dueLater();
     expect(dueLaterItems.length).toEqual(1);
   });
